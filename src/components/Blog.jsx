e4aa24924011c0a3cd796caf442f1f9571255fb9
davide <d.cannerozzi@gmail.com>
2022-07-04 12:46:42 +0200

    change blog article links

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,10 +4,11 @@ import SinglePost from './singlePost'
 
 export default function Blog(props) {
    const articles = [
-      { category:"react", description:"The useState Hook", link:"https://codingandbeers.com/usestate-hook-in-react/", icon:"fab fa-2x fa-react"},
-      { category:"react", description:"set up a react project with create-react-app", link:"https://codingandbeers.com/create-react-app/", icon:"fab fa-2x fa-react"},
-      { category:"react", description:"Array Destructuring in Javascript", link:"https://codingandbeers.com/destructuring-in-javascript/", icon:"fab fa-2x fa-react"},
-      { category:"css", description:"how to center a div with flexbox", link:"https://codingandbeers.com/flexbox-center-a-div/",icon:"fab fa-2x fa-css3-alt"}
+      { category:"react", description:"The useState Hook", link:"https://dev.to/davidecannerozzi/the-usestate-hook-471b", icon:"fab fa-2x fa-react"},
+      { category:"react", description:"set up a react project with create-react-app", link:"https://dev.to/davidecannerozzi/learn-create-react-app-a1j", icon:"fab fa-2x fa-react"},
+      { category:"react", description:"Array Destructuring in Javascript", link:"https://dev.to/davidecannerozzi/array-destructuring-ifo", icon:"fab fa-2x fa-react"},
+      { category:"css", description:"how to center a div with flexbox", link:"https://dev.to/davidecannerozzi/how-to-center-a-div-with-flexbox-2g73",icon:"fab fa-2x fa-css3-alt"},
+      { category:"html 5", description:"build a widget with <details> and <summary> tag", link:"https://dev.to/davidecannerozzi/html5-build-a-widget-with-and-tag-1a82", icon:"fab fa-2x fa-html5"}
    ]
    console.log()
    return (
@@ -16,7 +17,7 @@ export default function Blog(props) {
          <div className="header-blog w-72 mx-auto mt-4 mb-2">
             <h2 className="text-4xl p-2 relative">{props.header}</h2>
          </div>
-         <a href="https://codingandbeers.com/" className="underline block p-4" target="_blank" rel="noreferrer">{props.link}</a>
+         <a href="https://dev.to/davidecannerozzi" className="underline block p-4" target="_blank" rel="noreferrer">{props.link}</a>
          <article className="grid sm:grid-cols-2 grid-cols-1">
             { articles.map( article=> (<SinglePost category={article.category} description={article.description} link={article.link} icon={article.icon}/>))}
          </article>
