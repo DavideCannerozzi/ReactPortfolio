65f943f4f7556f97e000e5f0fd9d2cf2674dbadf
davide <d.cannerozzi@gmail.com>
2022-08-14 20:56:41 +0200

    add linkedin icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About(props) {
    return (
       <div className="absolute w-full">
          <h2 className="text-center text-4xl p-2">{ props.header }</h2>
-         <div className="about-container sm:p-0 p-4">
+         <div className="about-container sm:p-0 md:p-8">
             <div className="grid md:grid-cols-3 grid-cols">
                <div className="md:col-span-2 sm:p-8 p-0">
                   <p>{ props.aboutMeFirst }</p>
--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProfilePicture(props){
    return(
       <>
-         <img src={props.href} className="rounded-full shadow-xl mt-6" alt={props.alt} />
+         <img src={props.href} className="rounded-full shadow-xl mt-6 w-40 ml-12" alt={props.alt} />
       </>
    )
 }
